6565293554eeb7bbbb203c7dc050b7bddc80a03b
duhnuker <dinukanh@gmail.com>
2025-06-25 22:31:44 +1000

    MensSingles, responsive + styling done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,9 +5,9 @@ const Header = () => {
     return (
         <Box className="border-b border-zinc-800 bg-zinc-700 backdrop-blur-sm sticky top-0 z-10">
             <Container>
-                <Flex justify="between" align="center" className="py-3 px-4 sm:py-4 sm:px-0">
-                    <Heading size={{ initial: "5", sm: "6" }} as="h1" className="font-bold">
-                        <Link to="/" className="text-white no-underline">
+                <Flex justify="between" align="center" className="py-3 px-4">
+                    <Heading size={{ initial: "6", sm: "7" }} as="h1" className="font-bold">
+                        <Link to="/" className="text-white no-underline font-serif">
                             AOFever
                         </Link>
                     </Heading>
