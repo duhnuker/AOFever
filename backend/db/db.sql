078b2583c28244355fb15419578963211f61f6c2
duhnuker <dinukanh@gmail.com>
2025-07-07 23:21:29 +1000

    atp, wta data schema

--- backend/db/db.sql
+++ backend/db/db.sql
@@ -41,3 +41,44 @@ CREATE TABLE aus_open_womens_doubles (
     runners_up_names VARCHAR(100) NOT NULL,
     score VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE atp (
+    id SERIAL PRIMARY KEY,
+    tournament VARCHAR(100) NOT NULL,
+    date DATE NOT NULL,
+    series VARCHAR(50) NOT NULL,
+    court VARCHAR(25) NOT NULL,
+    surface VARCHAR(10) NOT NULL,
+    round VARCHAR(50) NOT NULL,
+    best_of INT NOT NULL,
+    player_1 VARCHAR(50) NOT NULL,
+    player_2 VARCHAR(50) NOT NULL,
+    winner VARCHAR(50) NOT NULL,
+    rank_1 INT NOT NULL,
+    rank_2 INT NOT NULL,
+    pts_1 INT NOT NULL,
+    pts_2 INT NOT NULL,
+    odd_1 DOUBLE PRECISION NOT NULL,
+    odd_2 DOUBLE PRECISION NOT NULL,
+    score VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE wta (
+    id SERIAL PRIMARY KEY,
+    tournament VARCHAR(100) NOT NULL,
+    date DATE NOT NULL,
+    court VARCHAR(25) NOT NULL,
+    surface VARCHAR(10) NOT NULL,
+    round VARCHAR(50) NOT NULL,
+    best_of INT NOT NULL,
+    player_1 VARCHAR(50) NOT NULL,
+    player_2 VARCHAR(50) NOT NULL,
+    winner VARCHAR(50) NOT NULL,
+    rank_1 INT NOT NULL,
+    rank_2 INT NOT NULL,
+    pts_1 INT NOT NULL,
+    pts_2 INT NOT NULL,
+    odd_1 DOUBLE PRECISION NOT NULL,
+    odd_2 DOUBLE PRECISION NOT NULL,
+    score VARCHAR(50) NOT NULL
+);
